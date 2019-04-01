4fb016236ab5b1f37b52cda8b48cc0a373be923f
Piotr Ferdynus <299244@pw.edu.pl>
2019-04-01 11:58:13 +0200

    Struktura funkcji simulate

--- simulate.c
+++ simulate.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "generation.h"
 
 void simulate(gen_t* gen0, int n, char* output, int* save){
-	;
+	
+	gen_t* thisGen = gen0;
 
-
-
-
-
+	while(thisGen->num < n){
+		//writePng(thisGen, output);
+		if(save[thisGen->num] == 1){
+			//writeTxt(thisGen);
+		}
+		//nextGen(thisGen);
+	}
 }
